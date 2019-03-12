5f49ec11abae9e94be1355f168f55f6d4c114e92
Andy Lester <andy@petdance.com>
2019-03-12 00:41:53 -0500

    Releasing 0.02

--- lib/SQL/Tiny.pm
+++ lib/SQL/Tiny.pm
@@ -10,11 +10,11 @@ SQL::Tiny - A very simple SQL-building library
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use parent 'Exporter';
 
